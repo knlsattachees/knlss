588e1572d98c8eeb682658c46397dafee66bb06c
knlsattachees <knlsattachees@gmail.com>
2024-07-16 23:32:13 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php
 session_start();
-include 'db.php';
-
-// Check if the user is logged in, if not then redirect to login page
-if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
-    header('Location: login.php');
-    exit;
+if (isset($_SESSION['username'])) {
+    header("Location: login.php");
+    exit();
+} else {
+    header("location: dashboard.php");
+    exit();
 }
-
-
+?>
